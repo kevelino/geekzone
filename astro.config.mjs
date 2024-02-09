995984d9ff24c8d60875ff545f3fb951e6e048f6
Kevin-Nengou <kevinnengou02@gmail.com>
2024-02-09 21:56:32 +0100

    added action

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://kevelino.github.io',
-	base: '/geekzone',
 	integrations: [
 		mdx(), 
 		sitemap(),
